23179b7c999c134f00b8de34bd153bc236efb9d5
Suraj Chakrapani <surajchakrapani@gmail.com>
2024-12-11 11:44:46 +0530

    Revert "Added pwa dependencies"
    This reverts commit b2e6a064fccfbc582e22145b7bbda1159a550d54.

--- src/main.ts
+++ src/main.ts
@@ -2,13 +2,8 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
 import { provideHttpClient, withFetch } from '@angular/common/http';
-import { isDevMode } from '@angular/core';
-import { provideServiceWorker } from '@angular/service-worker';
 
 bootstrapApplication(AppComponent, {
-  providers : [provideHttpClient(withFetch()), provideServiceWorker('ngsw-worker.js', {
-            enabled: !isDevMode(),
-            registrationStrategy: 'registerWhenStable:30000'
-          })]
+  providers : [provideHttpClient(withFetch())]
 } )
   .catch((err) => console.error(err));
