b2e6a064fccfbc582e22145b7bbda1159a550d54
Suraj Chakrapani <surajchakrapani@gmail.com>
2024-12-11 11:41:27 +0530

    Added pwa dependencies

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,13 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
 import { provideHttpClient, withFetch } from '@angular/common/http';
+import { isDevMode } from '@angular/core';
+import { provideServiceWorker } from '@angular/service-worker';
 
 bootstrapApplication(AppComponent, {
-  providers : [provideHttpClient(withFetch())]
+  providers : [provideHttpClient(withFetch()), provideServiceWorker('ngsw-worker.js', {
+            enabled: !isDevMode(),
+            registrationStrategy: 'registerWhenStable:30000'
+          })]
 } )
   .catch((err) => console.error(err));
